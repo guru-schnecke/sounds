ab5b10bb4a2b6aa6218aa789d55dcc10cfe97b6d
Schnecke <eiweala@live.com>
2020-07-29 15:14:52 +0800

    added add new artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,17 +1,20 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit]
+
   def index
     # Artist.find()
     @artists =Artist.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    # @artist = Artist.find(params[:id])
   end
 
 
   def edit
-  
+    # @artist = Artist.find(params[:id])
   end
+
   def update
   end
 
@@ -35,6 +38,10 @@ class ArtistsController < ApplicationController
   end
 
   private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name, :hometown, :location, :year,:age)
